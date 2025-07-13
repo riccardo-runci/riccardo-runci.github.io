ce154a95312db6491241cacb6b0a35bf3a8d1701
Riccardo Runci <riccardo.runci.93@gmail.com>
2025-07-14 00:15:40 +0200

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1320716298"
+    serviceWorkerVersion: "4040501929"
   }
 });
